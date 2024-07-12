0b9c71cee4d2eb33595532cd051b70b478b78c1d
cgadd21 <ctg7866@g.rit.edu>
2024-07-11 21:23:44 -0400

    aceternity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,41 @@
 import CopyButton from "@/components/copy-button";
-import { Github, Linkedin } from "lucide-react";
-import Link from "next/link";
+import { BackgroundBeams } from "@/components/ui/background-beams";
+import { TypewriterEffectSmooth } from "@/components/ui/typewriter-effect";
+
+const words = [
+  {
+    text: "View",
+  },
+  {
+    text: "my",
+  },
+  {
+    text: "README",
+    className: "text-blue-500 dark:text-blue-500",
+  },
+  {
+    text: "in",
+  },
+  {
+    text: "your",
+  },
+  {
+    text: "terminal.",
+    className: "text-blue-500 dark:text-blue-500",
+  },
+];
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col px-6 py-6">
-      <div className="flex flex-row items-center justify-between">
-        <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">
-          Cooper Gadd
-        </h1>
-        <div className="flex flex-row space-x-4">
-          <Link target="_blank" href="https://github.com/cooper-gadd">
-            <Github />
-          </Link>
-          <Link
-            className="no-underline"
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.linkedin.com/in/coopergadd/"
-          >
-            <Linkedin />
-          </Link>
-        </div>
-      </div>
-      <div className="flex flex-1 items-center justify-center gap-2">
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <TypewriterEffectSmooth words={words} />
+      <div className="flex flex-row justify-center">
         <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
-          curl cooper-gadd.io/api/resume
+          curl cooper-gadd.io/api/readme
         </code>
-        <CopyButton value="curl cooper-gadd.io/api/resume" />
+        <CopyButton value="curl cooper-gadd.io/api/readme" />
       </div>
+      <BackgroundBeams />
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
+import { default as flattenColorPalette } from "tailwindcss/lib/util/flattenColorPalette";
 
 const config = {
   darkMode: ["class"],
@@ -19,9 +19,6 @@ const config = {
       },
     },
     extend: {
-      fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
-      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
@@ -78,7 +75,18 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"), addVariablesForColors],
 } satisfies Config;
 
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val]),
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
 export default config;
