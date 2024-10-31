f9ac3943838f51c026ef37f5910d21b44f64c464
Cooper Gadd <126418690+cooper-gadd@users.noreply.github.com>
2024-10-30 23:54:37 -0400

    removed api stuff, just being cool

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,16 @@
-import CopyButton from "@/components/copy-button";
 import { BackgroundBeams } from "@/components/ui/background-beams";
 import { TypewriterEffectSmooth } from "@/components/ui/typewriter-effect";
 
 const words = [
-  { text: "Explore" },
-  { text: "my" },
-  { text: "stellar", className: "text-blue-500 dark:text-blue-500" },
-  { text: "journey", className: "text-blue-500 dark:text-blue-500" },
-  { text: "in" },
-  { text: "your" },
-  { text: "terminal!", className: "text-blue-500 dark:text-blue-500" },
+  { text: "Cooper" },
+  { text: "is" },
+  { text: "cool.", className: "text-blue-500 dark:text-blue-500" },
 ];
 
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       <TypewriterEffectSmooth words={words} />
-      <div className="flex flex-row justify-center gap-2">
-        <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
-          curl https://www.cooper-gadd.io/api/me
-        </code>
-        <CopyButton value="curl https://www.cooper-gadd.io/api/me" />
-      </div>
       <BackgroundBeams />
     </main>
   );
